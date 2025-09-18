d84fd874babf78620ae6da9750042f331626caca
Mark Murphy <murphyml207@potsdam.edu>
2025-09-18 14:30:11 -0400

    Add Ruby `while`, `until` nodes

--- src/node.rs
+++ src/node.rs
@@ -17,6 +17,8 @@ pub enum Expr {
     False,
     Nil,
     If(Box<If>),
+    While(Box<While>),
+    Until(Box<Until>),
     Else(Box<Else>),
 }
 
@@ -25,6 +27,18 @@ pub struct If {
     pub predicate: Expr,
     pub statements: Statements,
     pub subsequent: Subsequent
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct While {
+    pub predicate: Expr,
+    pub statements: Statements,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Until {
+    pub predicate: Expr,
+    pub statements: Statements,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
