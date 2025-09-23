c44dc05785c3e23a9128cebe00c84c0e3950110d
Mark Murphy <murphyml207@potsdam.edu>
2025-09-23 12:39:44 -0400

    Copy Instr enum

---
+++ crates/macro/src/lib.rs
@@ -0,0 +1,361 @@
+//! attribution: Substantially copied from `walrus-macro` crate from Walrus library.
+//! https://github.com/wasm-bindgen/walrus
+//! MIT licensed.
+
+#![recursion_limit = "256"]
+
+extern crate proc_macro;
+
+use self::proc_macro::TokenStream;
+use heck::ToSnakeCase;
+use proc_macro2::Span;
+use quote::quote;
+use syn::ext::IdentExt;
+use syn::parse::{Parse, ParseStream};
+use syn::punctuated::Punctuated;
+use syn::DeriveInput;
+use syn::Error;
+use syn::{parse_macro_input, Ident, Result, Token};
+
+#[proc_macro_attribute]
+pub fn wasm_instr(_attr: TokenStream, input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let variants = match get_enum_variants(&input) {
+        Ok(v) => v,
+        Err(e) => return e.to_compile_error().into(),
+    };
+
+    assert_eq!(input.ident.to_string(), "Instr");
+
+    let types = create_types(&input.attrs, &variants);
+    let builder = create_builder(&variants);
+
+    let expanded = quote! {
+        #types
+        #builder
+    };
+
+    TokenStream::from(expanded)
+}
+
+struct WasmVariant {
+    syn: syn::Variant,
+    opts: WasmVariantOpts,
+}
+
+#[derive(Default)]
+struct WasmVariantOpts {
+    display_name: Option<syn::Ident>,
+    display_extra: Option<syn::Ident>,
+    skip_builder: bool,
+}
+
+fn get_enum_variants(input: &DeriveInput) -> Result<Vec<WasmVariant>> {
+    let en = match &input.data {
+        syn::Data::Enum(en) => en,
+        syn::Data::Struct(_) => {
+            panic!("can only put #[wasm_instr] on an enum; found it on a struct")
+        }
+        syn::Data::Union(_) => {
+            panic!("can only put #[wasm_instr] on an enum; found it on a union")
+        }
+    };
+    en.variants
+        .iter()
+        .cloned()
+        .map(|mut variant| {
+            Ok(WasmVariant {
+                opts: syn::parse(wasm_attrs(&mut variant.attrs))?,
+                syn: variant,
+            })
+        })
+        .collect()
+}
+
+impl Parse for WasmVariantOpts {
+    fn parse(input: ParseStream) -> Result<Self> {
+        enum Attr {
+            DisplayName(syn::Ident),
+            DisplayExtra(syn::Ident),
+            SkipBuilder,
+        }
+
+        let attrs = Punctuated::<_, syn::token::Comma>::parse_terminated(input)?;
+        let mut ret = WasmVariantOpts::default();
+        for attr in attrs {
+            match attr {
+                Attr::DisplayName(ident) => ret.display_name = Some(ident),
+                Attr::DisplayExtra(ident) => ret.display_extra = Some(ident),
+                Attr::SkipBuilder => ret.skip_builder = true,
+            }
+        }
+        return Ok(ret);
+
+        impl Parse for Attr {
+            fn parse(input: ParseStream) -> Result<Self> {
+                let attr: Ident = input.parse()?;
+                if attr == "display_name" {
+                    input.parse::<Token![=]>()?;
+                    let name = input.call(Ident::parse_any)?;
+                    return Ok(Attr::DisplayName(name));
+                }
+                if attr == "display_extra" {
+                    input.parse::<Token![=]>()?;
+                    let name = input.call(Ident::parse_any)?;
+                    return Ok(Attr::DisplayExtra(name));
+                }
+                if attr == "skip_builder" {
+                    return Ok(Attr::SkipBuilder);
+                }
+                Err(Error::new(attr.span(), "unexpected attribute"))
+            }
+        }
+    }
+}
+
+fn wasm_attrs(attrs: &mut Vec<syn::Attribute>) -> TokenStream {
+    let mut ret = proc_macro2::TokenStream::new();
+    let ident = syn::Path::from(syn::Ident::new("walrus", Span::call_site()));
+    for i in (0..attrs.len()).rev() {
+        if attrs[i].path() != &ident {
+            continue;
+        }
+        let attr = attrs.remove(i);
+        let group = if let syn::Meta::List(syn::MetaList { tokens, .. }) = attr.meta {
+            tokens
+        } else {
+            panic!("#[walrus(...)] expected")
+        };
+        ret.extend(group);
+        ret.extend(quote! { , });
+    }
+    ret.into()
+}
+
+fn create_types(attrs: &[syn::Attribute], variants: &[WasmVariant]) -> impl quote::ToTokens {
+    let types: Vec<_> = variants
+        .iter()
+        .map(|v| {
+            let name = &v.syn.ident;
+            let attrs = &v.syn.attrs;
+            let fields = v.syn.fields.iter().map(|f| {
+                let name = &f.ident;
+                let attrs = &f.attrs;
+                let ty = &f.ty;
+                quote! {
+                    #( #attrs )*
+                    pub #name : #ty,
+                }
+            });
+            quote! {
+                #( #attrs )*
+                #[derive(Clone, Debug)]
+                pub struct #name {
+                    #( #fields )*
+                }
+
+                impl From<#name> for Instr {
+                    #[inline]
+                    fn from(x: #name) -> Instr {
+                        Instr::#name(x)
+                    }
+                }
+            }
+        })
+        .collect();
+
+    let methods: Vec<_> = variants
+        .iter()
+        .map(|v| {
+            let name = &v.syn.ident;
+            let snake_name = name.to_string().to_snake_case();
+
+            let is_name = format!("is_{}", snake_name);
+            let is_name = syn::Ident::new(&is_name, Span::call_site());
+
+            let ref_name = format!("{}_ref", snake_name);
+            let ref_name = syn::Ident::new(&ref_name, Span::call_site());
+
+            let mut_name = format!("{}_mut", snake_name);
+            let mut_name = syn::Ident::new(&mut_name, Span::call_site());
+
+            let unwrap_name = format!("unwrap_{}", snake_name);
+            let unwrap_name = syn::Ident::new(&unwrap_name, Span::call_site());
+
+            let unwrap_mut_name = format!("unwrap_{}_mut", snake_name);
+            let unwrap_mut_name = syn::Ident::new(&unwrap_mut_name, Span::call_site());
+
+            let ref_name_doc = format!(
+                "
+                If this instruction is a `{}`, get a shared reference to it.
+
+                Returns `None` otherwise.
+            ",
+                name
+            );
+
+            let mut_name_doc = format!(
+                "
+                If this instruction is a `{}`, get an exclusive reference to it.
+
+                Returns `None` otherwise.
+            ",
+                name
+            );
+
+            let is_name_doc = format!("Is this instruction a `{}`?", name);
+
+            let unwrap_name_doc = format!(
+                "
+                Get a shared reference to the underlying `{}`.
+
+                Panics if this instruction is not a `{}`.
+            ",
+                name, name
+            );
+
+            let unwrap_mut_name_doc = format!(
+                "
+                Get an exclusive reference to the underlying `{}`.
+
+                Panics if this instruction is not a `{}`.
+            ",
+                name, name
+            );
+
+            quote! {
+                #[doc=#ref_name_doc]
+                #[inline]
+                fn #ref_name(&self) -> Option<&#name> {
+                    if let Instr::#name(ref x) = *self {
+                        Some(x)
+                    } else {
+                        None
+                    }
+                }
+
+                #[doc=#mut_name_doc]
+                #[inline]
+                pub fn #mut_name(&mut self) -> Option<&mut #name> {
+                    if let Instr::#name(ref mut x) = *self {
+                        Some(x)
+                    } else {
+                        None
+                    }
+                }
+
+                #[doc=#is_name_doc]
+                #[inline]
+                pub fn #is_name(&self) -> bool {
+                    self.#ref_name().is_some()
+                }
+
+                #[doc=#unwrap_name_doc]
+                #[inline]
+                pub fn #unwrap_name(&self) -> &#name {
+                    self.#ref_name().unwrap()
+                }
+
+                #[doc=#unwrap_mut_name_doc]
+                #[inline]
+                pub fn #unwrap_mut_name(&mut self) -> &mut #name {
+                    self.#mut_name().unwrap()
+                }
+            }
+        })
+        .collect();
+
+    let variants: Vec<_> = variants
+        .iter()
+        .map(|v| {
+            let name = &v.syn.ident;
+            let attrs = &v.syn.attrs;
+            quote! {
+                #( #attrs )*
+                #name(#name)
+            }
+        })
+        .collect();
+
+    quote! {
+        #( #types )*
+
+        #( #attrs )*
+        pub enum Instr {
+            #(#variants),*
+        }
+
+        impl Instr {
+            #( #methods )*
+        }
+    }
+}
+
+fn create_builder(variants: &[WasmVariant]) -> impl quote::ToTokens {
+    let mut builder_methods = Vec::new();
+    for variant in variants {
+        if variant.opts.skip_builder {
+            continue;
+        }
+
+        let name = &variant.syn.ident;
+
+        let mut method_name = name.to_string().to_snake_case();
+
+        let mut method_name_at = method_name.clone();
+        method_name_at.push_str("_at");
+        let method_name_at = syn::Ident::new(&method_name_at, Span::call_site());
+
+        if method_name == "return" || method_name == "const" {
+            method_name.push('_');
+        } else if method_name == "block" {
+            continue;
+        }
+        let method_name = syn::Ident::new(&method_name, Span::call_site());
+
+        let mut args = Vec::new();
+        let mut arg_names = Vec::new();
+
+        for field in variant.syn.fields.iter() {
+            let name = field.ident.as_ref().expect("can't have unnamed fields");
+            arg_names.push(name);
+            let ty = &field.ty;
+            args.push(quote! { #name: #ty });
+        }
+
+        let doc = format!(
+            "Push a new `{}` instruction onto this builder's block.",
+            name
+        );
+        let at_doc = format!(
+            "Splice a new `{}` instruction into this builder's block at the given index.\n\n\
+             # Panics\n\n\
+             Panics if `position > self.instrs.len()`.",
+            name
+        );
+
+        let arg_names = &arg_names;
+        let args = &args;
+
+        builder_methods.push(quote! {
+            #[inline]
+            #[doc=#doc]
+            pub fn #method_name(&mut self, #(#args),*) -> &mut Self {
+                self.instr(#name { #(#arg_names),* })
+            }
+
+            #[inline]
+            #[doc=#at_doc]
+            pub fn #method_name_at(&mut self, position: usize, #(#args),*) -> &mut Self {
+                self.instr_at(position, #name { #(#arg_names),* })
+            }
+        });
+    }
+    quote! {
+        #[allow(missing_docs)]
+        impl crate::InstrSeqBuilder<'_> {
+            #(#builder_methods)*
+        }
+    }
+}
--- src/wasm.rs
+++ src/wasm.rs
@@ -1,6 +1,10 @@
-//! A representation of the _compiled_ wasm program.
-//! Should be trivially convertible to .wat
-//! NB: Strange naming conventions are mostly copied from Wasm spec.
+//! An AST representation of the final Wasm code.
+//! One style of Wasm's text representation is tree-structured s-expressions,
+//!     so this can be trivially written to the `.wat` format.
+//!
+//! attribution: Substantially copied from Walrus `walrus/src/ir/mod.rs`
+//! https://github.com/wasm-bindgen/walrus
+//! MIT licensed
 
 pub mod types;
 pub mod values;
@@ -8,7 +12,396 @@ pub mod values;
 use crate::wasm::types::{GlobalType, Type, ValueType};
 use crate::wasm::values::{I32, I64, U32};
 
-// ==== Wasm Instructions ====
+/// An enum of all the different kinds of wasm instructions.
+///
+/// Note that the `#[walrus_expr]` macro rewrites this enum's variants from
+///
+/// ```ignore
+/// enum Instr {
+///     Variant { field: Ty, .. },
+///     ...
+/// }
+/// ```
+///
+/// into
+///
+/// ```ignore
+/// enum Instr {
+///     Variant(Variant),
+///     ...
+/// }
+///
+/// struct Variant {
+///     field: Ty,
+///     ...
+/// }
+/// ```
+// #[walrus_instr]
+#[derive(Clone, Debug)]
+pub enum Instr {
+    // /// `block ... end`
+    // #[walrus(skip_builder)]
+    // Block {
+    //     /// The id of this `block` instruction's inner `InstrSeq`.
+    //     seq: InstrSeqId,
+    // },
+    //
+    // /// `loop ... end`
+    // #[walrus(skip_builder)]
+    // Loop {
+    //     /// The id of this `loop` instruction's inner `InstrSeq`.
+    //     seq: InstrSeqId,
+    // },
+    //
+    // /// `call`
+    // Call {
+    //     /// The function being invoked.
+    //     func: FunctionId,
+    // },
+    //
+    // /// `call_indirect`
+    // CallIndirect {
+    //     /// The type signature of the function we're calling
+    //     ty: TypeId,
+    //     /// The table which `func` below is indexing into
+    //     table: TableId,
+    // },
+    //
+    // /// `local.get n`
+    // LocalGet {
+    //     /// The local being got.
+    //     local: LocalId,
+    // },
+    //
+    // /// `local.set n`
+    // LocalSet {
+    //     /// The local being set.
+    //     local: LocalId,
+    // },
+    //
+    // /// `local.tee n`
+    // LocalTee {
+    //     /// The local being set.
+    //     local: LocalId,
+    // },
+    //
+    // /// `global.get n`
+    // GlobalGet {
+    //     /// The global being got.
+    //     global: GlobalId,
+    // },
+    //
+    // /// `global.set n`
+    // GlobalSet {
+    //     /// The global being set.
+    //     global: GlobalId,
+    // },
+    //
+    // /// `*.const`
+    // Const {
+    //     /// The constant value.
+    //     value: Value,
+    // },
+    //
+    // /// Ternary operations, those requiring three operands
+    // TernOp {
+    //     /// The operation being performed
+    //     #[walrus(skip_visit)]
+    //     op: TernaryOp,
+    // },
+    //
+    // /// Binary operations, those requiring two operands
+    // Binop {
+    //     /// The operation being performed
+    //     #[walrus(skip_visit)]
+    //     op: BinaryOp,
+    // },
+    //
+    // /// Unary operations, those requiring one operand
+    // Unop {
+    //     /// The operation being performed
+    //     #[walrus(skip_visit)]
+    //     op: UnaryOp,
+    // },
+    //
+    // /// `select`
+    // Select {
+    //     /// Optionally listed type that the `select` instruction is expected to
+    //     /// produce, used in subtyping relations with the gc proposal.
+    //     #[walrus(skip_visit)]
+    //     ty: Option<ValType>,
+    // },
+    //
+    // /// `unreachable`
+    // Unreachable {},
+    //
+    // /// `br`
+    // Br {
+    //     /// The target block to branch to.
+    //     #[walrus(skip_visit)] // should have already been visited
+    //     block: InstrSeqId,
+    // },
+    //
+    // /// `br_if`
+    // BrIf {
+    //     /// The target block to branch to when the condition is met.
+    //     #[walrus(skip_visit)] // should have already been visited
+    //     block: InstrSeqId,
+    // },
+    //
+    // /// `if <consequent> else <alternative> end`
+    // #[walrus(skip_builder)]
+    // IfElse {
+    //     /// The block to execute when the condition is true.
+    //     consequent: InstrSeqId,
+    //     /// The block to execute when the condition is false.
+    //     alternative: InstrSeqId,
+    // },
+    //
+    // /// `br_table`
+    // BrTable {
+    //     /// The table of target blocks.
+    //     #[walrus(skip_visit)] // should have already been visited
+    //     blocks: Box<[InstrSeqId]>,
+    //     /// The block that is branched to by default when `which` is out of the
+    //     /// table's bounds.
+    //     #[walrus(skip_visit)] // should have already been visited
+    //     default: InstrSeqId,
+    // },
+    //
+    // /// `drop`
+    // Drop {},
+    //
+    // /// `return`
+    // Return {},
+    //
+    // /// `memory.size`
+    // MemorySize {
+    //     /// The memory we're fetching the current size of.
+    //     memory: MemoryId,
+    // },
+    //
+    // /// `memory.grow`
+    // MemoryGrow {
+    //     /// The memory we're growing.
+    //     memory: MemoryId,
+    // },
+    //
+    // /// `memory.init`
+    // MemoryInit {
+    //     /// The memory we're growing.
+    //     memory: MemoryId,
+    //     /// The data to copy in
+    //     data: DataId,
+    // },
+    //
+    // /// `data.drop`
+    // DataDrop {
+    //     /// The data to drop
+    //     data: DataId,
+    // },
+    //
+    // /// `memory.copy`
+    // MemoryCopy {
+    //     /// The source memory
+    //     src: MemoryId,
+    //     /// The destination memory
+    //     dst: MemoryId,
+    // },
+    //
+    // /// `memory.fill`
+    // MemoryFill {
+    //     /// The memory to fill
+    //     memory: MemoryId,
+    // },
+    //
+    // /// `*.load`
+    // ///
+    // /// Loading a value from memory.
+    // Load {
+    //     /// The memory we're loading from.
+    //     memory: MemoryId,
+    //     /// The kind of memory load this is performing
+    //     #[walrus(skip_visit)]
+    //     kind: LoadKind,
+    //     /// The alignment and offset of this memory load
+    //     #[walrus(skip_visit)]
+    //     arg: MemArg,
+    // },
+    //
+    // /// `*.store`
+    // ///
+    // /// Storing a value to memory.
+    // Store {
+    //     /// The memory we're storing to
+    //     memory: MemoryId,
+    //     /// The kind of memory store this is performing
+    //     #[walrus(skip_visit)]
+    //     kind: StoreKind,
+    //     /// The alignment and offset of this memory store
+    //     #[walrus(skip_visit)]
+    //     arg: MemArg,
+    // },
+    //
+    // /// An atomic read/modify/write operation.
+    // AtomicRmw {
+    //     /// The memory we're modifying
+    //     memory: MemoryId,
+    //     /// The atomic operation being performed
+    //     #[walrus(skip_visit)]
+    //     op: AtomicOp,
+    //     /// The atomic operation being performed
+    //     #[walrus(skip_visit)]
+    //     width: AtomicWidth,
+    //     /// The alignment and offset from the base address
+    //     #[walrus(skip_visit)]
+    //     arg: MemArg,
+    // },
+    //
+    // /// An atomic compare-and-exchange operation.
+    // Cmpxchg {
+    //     /// The memory we're modifying
+    //     memory: MemoryId,
+    //     /// The atomic operation being performed
+    //     #[walrus(skip_visit)]
+    //     width: AtomicWidth,
+    //     /// The alignment and offset from the base address
+    //     #[walrus(skip_visit)]
+    //     arg: MemArg,
+    // },
+    //
+    // /// The `atomic.notify` instruction to wake up threads.
+    // AtomicNotify {
+    //     /// The memory we're notifying through
+    //     memory: MemoryId,
+    //     /// The alignment and offset from the base address
+    //     #[walrus(skip_visit)]
+    //     arg: MemArg,
+    // },
+    //
+    // /// The `*.atomic.wait` instruction to block threads.
+    // AtomicWait {
+    //     /// The memory we're waiting through.
+    //     memory: MemoryId,
+    //     /// The alignment and offset from the base address.
+    //     #[walrus(skip_visit)]
+    //     arg: MemArg,
+    //     /// Whether or not this is an `i32` or `i64` wait.
+    //     #[walrus(skip_visit)]
+    //     sixty_four: bool,
+    // },
+    //
+    // /// The `atomic.fence` instruction
+    // AtomicFence {},
+    //
+    // /// `table.get`
+    // TableGet {
+    //     /// The table we're fetching from.
+    //     table: TableId,
+    // },
+    //
+    // /// `table.set`
+    // TableSet {
+    //     /// The table we're storing to.
+    //     table: TableId,
+    // },
+    //
+    // /// `table.grow`
+    // TableGrow {
+    //     /// The table we're growing
+    //     table: TableId,
+    // },
+    //
+    // /// `table.size`
+    // TableSize {
+    //     /// The table we're getting the size of
+    //     table: TableId,
+    // },
+    //
+    // /// `table.fill`
+    // TableFill {
+    //     /// The table we're filling
+    //     table: TableId,
+    // },
+    //
+    // /// `ref.null $ty`
+    // RefNull {
+    //     /// The type of null that we're producing
+    //     #[walrus(skip_visit)]
+    //     ty: RefType,
+    // },
+    //
+    // /// `ref.is_null`
+    // RefIsNull {},
+    //
+    // /// `ref.func`
+    // RefFunc {
+    //     /// The function that this instruction is referencing
+    //     func: FunctionId,
+    // },
+    //
+    // /// `v128.bitselect`
+    // V128Bitselect {},
+    //
+    // /// `i8x16.swizzle`
+    // I8x16Swizzle {},
+    //
+    // /// `i8x16.shuffle`
+    // I8x16Shuffle {
+    //     /// The indices that are used to create the final vector of this
+    //     /// instruction
+    //     #[walrus(skip_visit)]
+    //     indices: ShuffleIndices,
+    // },
+    //
+    // /// Various instructions to load a simd vector from memory
+    // LoadSimd {
+    //     /// The memory we're loading from.
+    //     memory: MemoryId,
+    //     /// The size of load this is performing
+    //     #[walrus(skip_visit)]
+    //     kind: LoadSimdKind,
+    //     /// The alignment and offset of this memory load
+    //     #[walrus(skip_visit)]
+    //     arg: MemArg,
+    // },
+    //
+    // /// `table.init`
+    // TableInit {
+    //     /// The table we're copying into.
+    //     table: TableId,
+    //     /// The element we're getting items from.
+    //     elem: ElementId,
+    // },
+    //
+    // /// `elem.drop`
+    // ElemDrop {
+    //     /// The elem segment to drop
+    //     elem: ElementId,
+    // },
+    //
+    // /// `table.copy`
+    // TableCopy {
+    //     /// The source table
+    //     src: TableId,
+    //     /// The destination table
+    //     dst: TableId,
+    // },
+    //
+    // /// `return_call`
+    // ReturnCall {
+    //     /// The function being invoked.
+    //     func: FunctionId,
+    // },
+    //
+    // /// `return_call_indirect`
+    // ReturnCallIndirect {
+    //     /// The type signature of the function we're calling
+    //     ty: TypeId,
+    //     /// The table which `func` below is indexing into
+    //     table: TableId,
+    // },
+}
 
 #[derive(Debug, Clone)]
 pub enum Instruction {
